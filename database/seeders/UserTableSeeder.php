e2db238476ca03ddf6a3f9b304271319af148941
Steven Abbott <abbottwilliams.steven@gmail.com>
2022-11-02 22:00:56 +0000

    Created a User seeder and hard coded an example

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create a user
+        $a = new User();
+        $a -> name = 'John Doe';    
+        $a -> email = 'john.doe@example.com';
+        $a -> password = bcrypt('password');
+        $a -> save();
+    
+    }
+}
