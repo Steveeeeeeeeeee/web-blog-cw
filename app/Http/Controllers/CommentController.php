c1e5bcf4205907004e1bfa29647edd1499b133a4
Steven Abbott <abbottwilliams.steven@gmail.com>
2022-11-04 00:35:45 +0000

    Comment controller now shows all comments under a post

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Comment;
+use App\Models\Posts;   
 class CommentController extends Controller
 {
     //
     public function store(Request $request)
     {
         // store code
-        $comment = new Comment();
-        $comment->body = $request->get('body');
-        $comment->user_id = Auth::user()->id;
-        $comment->post_id = $request->get('post_id');
-        $comment->comments()->save($comment);
+        $comment = new Comment();   
+        $comment->body = $request->body;    
+        //get the user id
+        $comment->user_id = auth()->user()->id; 
+        //get the post id
+        $comment->posts_id = $request->post_id;
+        $comment->parent_id = $request->parent_id;
+        $comment->save();   
 
         return redirect()->back();
+
          
     }
 }
--- resources/views/blog/postID.blade.php
+++ resources/views/blog/postID.blade.php
@@ -4,12 +4,12 @@
             {{ __('Posts') }}
         </h2>
     </x-slot>
-    <div class="py-12">
+    <div class="py-12 bg-zinc-600">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="bg-slate-500 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-slate-600 border-b border-gray-200">
                     <!--return post with ID -->
-                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="p-6 sm:px-20 bg-slate-600 border-b border-gray-200">
                         <div class="mt-8 text-2xl">
                             {{ $post->title }}
                         </div>
@@ -20,8 +20,24 @@
                             {{ $post->user->name }}
                         </div>
                     </div>
-                    <!-- add comment form -->
-                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                   
+                    <!-- show comments -->
+                    <div class="p-6 sm:px-20 bg-slate-600 border-b border-gray-200">
+                        <div class="emt-8 text-2xl">
+                            Comments : 
+                        </div>
+                        @foreach($post->comments as $comment)
+                        <div class="text-lg ml-7 mt-8 text-blue-500">
+                            {{ $comment->user->name }}
+                            <div class="text-sm ml-7 mt-6 text-gray-500">
+                            {{ $comment->body }}
+                        </div>
+                        </div>
+                        
+                        @endforeach
+                    </div>
+                     <!-- add comment form -->
+                     <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
                         <div class="mt-8 text-2xl">
                             Add Comment
                         </div>
@@ -33,20 +49,6 @@
                                 <button type="submit">Add Comment</button>
                             </form>
                         </div>
-                    </div>
-                    <!-- show comments -->
-                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                        <div class="mt-8 text-2xl">
-                            Comments
-                        </div>
-                        @foreach($post->comments as $comment)
-                        <div class="mt-6 text-gray-500">
-                            {{ $comment->body }}
-                        </div>
-                        <div class="mt-6 text-gray-500">
-                            {{ $comment->user->name }}
-                        </div>
-                        @endforeach
                     </div>
                 
                 
