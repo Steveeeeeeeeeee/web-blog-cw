2e16eb37d8d289d5c44059fdda0d02329070d609
Steven Abbott <abbottwilliams.steven@gmail.com>
2022-11-05 00:17:34 +0000

    setup tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        './src/**/*{.html,js}',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/views/**/**/*.blade.php', 
+        './resources/views/**/*.blade.php'
+        
     ],
 
     theme: {
