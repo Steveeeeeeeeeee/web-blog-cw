55568169fb173f18a9e345b0ec7ab4ce54dae25f
Steven Abbott <abbottwilliams.steven@gmail.com>
2022-11-05 02:03:29 +0000

    Changed the styling on the postID.blade.php file

--- resources/views/blog/postID.blade.php
+++ resources/views/blog/postID.blade.php
@@ -5,53 +5,56 @@
             {{ __('Posts') }}
         </h2>
     </x-slot>
+   
     <head>
         <title>Posts</title>
         @vite('resources/css/app.css')
     </head>
-<body class="bg-slate-200 font-sans leading-normal tracking-normal">
+    
+<body class="bg-slate-700 font-sans leading-normal tracking-normal">
     <!-- create a nice looking blog post with the post and the comments to the side -->
-    <div class="flex flex-col md:flex-row">
+    <div class="flex flex-col md:flex-row justify-center bg-gray-700 h-fit">
         <!-- post -->
-        <div class="w-full md:w-3/5 bg-white mx-2 md:mx-0">
+        <div class="w-full md:w-3/5 bg-gray-700 mx-2 md:mx-0 mt-6 border-black ">
             <!-- post title -->
-            <div class="bg-white w-full p-4">
-                <div class="text-3xl font-bold text-gray-800">
+            <div class="bg-gray-800 w-full p-4 border-black rounded-lg">
+                <div class="text-3xl font-bold text-white">
                     {{ $post->title }}
                 </div>
-                <div class="text-sm font-bold text-gray-600">
-                    Posted by {{ $post->user->name }}
+                <div class="text-sm font-bold text-yellow-500">
+                    Posted by {{ $post->user->name }} on {{ $post->created_at->format('d/m/Y') }}
                 </div>
             </div>
             <!-- post body -->
-            <div class="bg-white w-full p-4">
-                <div class="text-xl text-gray-800">
+            <div class="bg-white w-full p-4 my-5 border-black rounded-md justify-center">
+                <div class="text-xl text-black">
                     {{ $post->body }}
                 </div>
             </div>
             <!-- post comments -->
-            <div class="bg-white w-full p-4">
-                <div class="text-2xl font-bold text-gray-800">
-                    Comments
+            <div class="bg-gray-600 w-full p-4 rounded-lg">
+                <div class="text-2xl font-bold text-gray-800 border-black rounded-lg">
+                    Comments: {{ $post->comments->count() }}
                 </div>
                 @foreach($post->comments as $comment)
-                <div class="flex flex-col md:flex-row border-2 border-gray-200 rounded-lg p-4 my-2">
+                <div class="flex flex-col md:flex-row bg-white border-2 border-slate-900 rounded-lg p-4 my-2">
                     <div class="flex-1">
-                        <div class="text-lg font-bold text-gray-800">
+                        <div class="text-lg font-bold text-gray-900">
                             {{ $comment->user->name }}
                         </div>
                         <div class="text-gray-700">
                             {{ $comment->body }}
                         </div>
                     </div>
-                    <div class="flex-1 text-right">
+                    <div class="flex-1 text-right ">
                         <div class="text-sm font-bold text-gray-600">
                             {{ $comment->created_at->diffForHumans() }}
                         </div>
                         @if($comment->user_id == Auth::user()->id)
-                        <div class="flex flex-row-reverse">
+                        <div class="flex flex-row-reverse space-x-3">
+                            <!-- edit button edits on current webpage -->
                             <a href="{{ route('comment.edit', $comment->id) }}">
-                                <button class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+                                <button class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded mx-2">
                                     Edit
                                 </button>
                             </a>
@@ -69,24 +72,26 @@
                 @endforeach
             </div>
             <!-- add comment form -->
-            <div class="bg-white w-full p-4">
-                <div class="text-2xl font-bold text-gray-800">
+            <div class="bg-white w-5/6 p-4 rounded-lg mt-2 justify-center">
+                <div class="text-2xl ml-4 font-bold text-gray-800">
                     Add Comment
                 </div>
                 <form action="{{ route('comment.store') }}" method="POST">
                     @csrf
                     <input type="hidden" name="post_id" value="{{ $post->id }}">
-                    <div class="flex flex-col md:flex-row">
+                    <div class="flex flex-col md:flex-row justify-center">
                         <div class="flex-1">
-                            <textarea name="body" class="w-full p-4 border-2 border-gray-200 rounded-lg" placeholder="Comment"></textarea>
+                            <textarea name="body" class="w-full h-16 p-4 border-2 border-gray-200 rounded-lg" placeholder="Comment"></textarea>
                         </div>
-                        <div class="flex-1 bg-black text-black">
-                            <button type="submit" class="w-full p-4 bg-blue-500 hover:bg-blue-700 text-black font-bold rounded-lg">
+                        <div class="flex-1 text-black">
+                            <button type="submit" class="w-5/6 h-16 ml-4 p-4  bg-blue-500 hover:bg-blue-700 text-black font-bold rounded-lg">
                                 Add Comment
                             </button>
                         </div>
                     </div>
             </form>
+            </div>
+
 </body>
 </x-app-layout>
   
