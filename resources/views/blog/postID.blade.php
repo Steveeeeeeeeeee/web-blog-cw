233bbe00d90da93c4df29849a168fa0c76522f63
Steven Abbott <abbottwilliams.steven@gmail.com>
2022-11-05 00:56:33 +0000

    post with id now supports various features

--- resources/views/blog/postID.blade.php
+++ resources/views/blog/postID.blade.php
@@ -1,80 +1,99 @@
+<!doctype html>
 <x-app-layout>
 <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Posts') }}
         </h2>
     </x-slot>
-    <div class="py-12 bg-zinc-600">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-slate-500 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-slate-600 border-b border-gray-200">
-                    <!--return post with ID -->
-                    <div class="p-6 sm:px-20 bg-slate-600 border-b border-gray-200">
-                        <div class="mt-8 text-2xl">
-                            {{ $post->title }}
-                        </div>
-                        <div class="mt-6 text-gray-500">
-                            {{ $post->body }}
-                        </div>
-                        <div class="mt-6 text-gray-500">
-                            {{ $post->user->name }}
-                        </div>
-                    </div>
-                   
-                    <!-- show comments and indent the comment from the user name-->
-                    <div class="p-6 sm:px-20 bg-slate-600 border-b border-gray-200">
-                        <div class="mt-8 text-2xl">
-                            Comments
-                        </div>
-                        <div class="mt-6 text-gray-500">
-                            <!-- add an edit & delete button if the comment is the users -->
-                            @foreach($post->comments as $comment)
-                            <!-- tailwind class that overrides layouts -->
-                            <div class="bg-red-500">
-                                <div class="mt-6 text-gray-500">
-                                    <div class="mt-6 text-xl text-gray-500">
-                                        {{ $comment->user->name }}
-                                    </div>
-                                    <div class="mt-6 text-gray-500">
-                                        {{ $comment->body }}
-                                    </div>
-                                    @if($comment->user_id == Auth::user()->id)
-                                        <div class="mt-6 text-gray-500">
-                                            <!-- edit button opens a text box -->
-                                            <a href="{{ route('comment.edit', $comment->id) }}">Edit</a>
-                                            <form action="{{ route('comment.destroy', $comment->id) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit">Delete</button>
-                                            </form>
-                                        </div>
-                
-                                    @endif
-                                </div>
-                            </div>  
-                         
-                            @endforeach
-                        </div>
-                    
-                    </div>
-                     <!-- add comment form -->
-                     <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                        <div class="mt-8 text-2xl">
-                            Add Comment
-                        </div>
-                        <div class="mt-6 text-gray-500">
-                            <form method="POST" action="{{ route('comment.store') }}">
-                                @csrf
-                                <input type="hidden" name="post_id" value="{{ $post->id }}">
-                                <textarea name="body" cols="30" rows="10"></textarea>
-                                <button type="submit">Add Comment</button>
-                            </form>
-                        </div>
-                    </div>
-                
-                
+    <head>
+        <title>Posts</title>
+        @vite('resources/css/app.css')
+    </head>
+<body class="bg-slate-200 font-sans leading-normal tracking-normal">
+    <!-- create a nice looking blog post with the post and the comments to the side -->
+    <div class="flex flex-col md:flex-row">
+        <!-- post -->
+        <div class="w-full md:w-3/5 bg-white mx-2 md:mx-0">
+            <!-- post title -->
+            <div class="bg-white w-full p-4">
+                <div class="text-3xl font-bold text-gray-800">
+                    {{ $post->title }}
+                </div>
+                <div class="text-sm font-bold text-gray-600">
+                    Posted by {{ $post->user->name }}
                 </div>
             </div>
-        </div>
-    </div>
+            <!-- post body -->
+            <div class="bg-white w-full p-4">
+                <div class="text-xl text-gray-800">
+                    {{ $post->body }}
+                </div>
+            </div>
+            <!-- post comments -->
+            <div class="bg-white w-full p-4">
+                <div class="text-2xl font-bold text-gray-800">
+                    Comments
+                </div>
+                @foreach($post->comments as $comment)
+                <div class="flex flex-col md:flex-row border-2 border-gray-200 rounded-lg p-4 my-2">
+                    <div class="flex-1">
+                        <div class="text-lg font-bold text-gray-800">
+                            {{ $comment->user->name }}
+                        </div>
+                        <div class="text-gray-700">
+                            {{ $comment->body }}
+                        </div>
+                    </div>
+                    <div class="flex-1 text-right">
+                        <div class="text-sm font-bold text-gray-600">
+                            {{ $comment->created_at->diffForHumans() }}
+                        </div>
+                        @if($comment->user_id == Auth::user()->id)
+                        <div class="flex flex-row-reverse">
+                            <a href="{{ route('comment.edit', $comment->id) }}">
+                                <button class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+                                    Edit
+                                </button>
+                            </a>
+                            <form action="{{ route('comment.destroy', $comment->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="bg-red-500 hover:bg-red-700 text-black font-bold py-2 px-4 rounded">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
+                        @endif
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <!-- add comment form -->
+            <div class="bg-white w-full p-4">
+                <div class="text-2xl font-bold text-gray-800">
+                    Add Comment
+                </div>
+                <form action="{{ route('comment.store') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                    <div class="flex flex-col md:flex-row">
+                        <div class="flex-1">
+                            <textarea name="body" class="w-full p-4 border-2 border-gray-200 rounded-lg" placeholder="Comment"></textarea>
+                        </div>
+                        <div class="flex-1 bg-black text-black">
+                            <button type="submit" class="w-full p-4 bg-blue-500 hover:bg-blue-700 text-black font-bold rounded-lg">
+                                Add Comment
+                            </button>
+                        </div>
+                    </div>
+            </form>
+</body>
 </x-app-layout>
+  
+
+        
+
+
+
+        <!-- create a column for the posts -->
+    
